1377affbc9eb6c7b1255b089eee892a783592a43
VyLe <thaovyletruong@gmail.com>
2019-12-06 18:42:04 -0600

    Style the game

---
+++ script.js
@@ -0,0 +1,4 @@
+var quesAns = [{
+  ques: "The Smith family is a very wealthy family that lives in a big, circular home. One morning, Mr. Smith woke up and saw a strawberry jam stain on his new carpet. He figured out that everyone who was there that morning had a jam sandwich. Billy Smith: \“I was outside playing basketball.\”The Maid: \“I was dusting the corners of the house.\”Chef: \“I was starting to make lunch for later.”Who is lying?\"",
+  ans: ["Billy", "Maid", "Chef"]
+}]
