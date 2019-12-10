1c769191567a4aaddfc9c0167cfa044150cb9e05
VyLe <thaovyletruong@gmail.com>
2019-12-10 03:29:07 -0600

    logic

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 fetch("./qus.json")
   .then(response => response.json())
-  .then(data => displayQus(data))
+  .then(data => start(data))
   .catch(err => console.log("err", err))
 
 //------------------------------------ Start Game
@@ -15,23 +15,41 @@ window.onclick = function () {
 }
 
 //------------------------------------- Game play
-
-function displayQus(data) {
+var alreadyId = []
+//......................... grab some info for id & qus the First time
+function start(data) {
   var dataText = data
   var id = Math.floor(Math.random() * dataText.length - 1);
   var qusText = document.querySelector('.ques-container h4')
-  qusText.innerText = dataText[id].qus
-  console.log(dataText[id].qus)
-  displayAns(dataText, id)
+  displayQus(dataText, id, qusText)
+}
+//........................................ display qus -> Second time start here
+function displayQus(dataText, id, qusText) {
+  dataText = dataText
+  qusText = qusText
+  document.querySelector('.conclusion-container').style.display = "block"
+  document.querySelector('.check').style.opacity = 0
+
+  id = Math.floor(Math.random() * dataText.length - 1);
+  if (!alreadyId.includes(id)) {
+    qusText.innerText = dataText[id].qus
+    alreadyId.push(id)
+    displayAns(dataText, id)
+    console.log("old id", alreadyId)
+  } else {
+
+    displayQus(dataText, id, qusText)
+  }
 }
 
+//............................................... display ans (grab some info)
 function displayAns(dataText, id) {
   dataText = dataText
   id = id
-  console.log(id)
   var ansContainer = document.querySelector('.ans-container')
-  for (let i = 0; i < dataText[id].quotes.length; i++) {
 
+  //...............................................  start to display ans
+  for (let i = 0; i < dataText[id].quotes.length; i++) {
     var ans = document.createElement('article')
     ans.setAttribute('class', 'ans')
     ansContainer.appendChild(ans)
@@ -43,14 +61,38 @@ function displayAns(dataText, id) {
   }
   var ansArr = document.querySelectorAll('.ans')
   var quoteText = document.querySelectorAll('h5')
+  //............................................... ans on click
   for (let i = 0; i < ansArr.length; i++) {
     ansArr[i].addEventListener('click', function () {
       if (quoteText[i].textContent == dataText[id].ans) {
-        console.log('correct')
+        document.querySelector('.right-wrong').innerText = "Genius!"
+        reset(id, dataText)
       } else {
-        console.log("wrong")
+        document.querySelector('.right-wrong').innerText = "Incorrect"
+        reset(id, dataText)
       }
     })
+  }
+}
+
+function reset(id, dataText) {
+  document.querySelector('.conclusion-container').style.display = "none"
+  document.querySelector('.check').style.opacity = 1
+
+  dataText = dataText
+  alreadyId = alreadyId
+  alreadyId.push(id)
+  for (var i = 0; i < alreadyId.length; i++) {
+    id = Math.floor(Math.random() * dataText.length - alreadyId[i])
+  }
+  var qusText = document.querySelector('.ques-container h4')
+  if (alreadyId.length < dataText.length) {
+    this.setTimeout(function () {
+      document.querySelectorAll('.ans').forEach(e => e.remove())
+      displayQus(dataText, id, qusText)
+    }, 1000)
+  } else {
+    qusText.innerText = "Congrats! You have completes all the riddles. You are a genius!"
   }
 }
 
