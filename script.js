36bdcf59240308e4f6979dc37980a541bf01d6c8
VyLe <thaovyletruong@gmail.com>
2019-12-09 22:19:29 -0600

    Json test

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-var quesAns = [{
-  title: "",
-  ques: "The Smith family is a very wealthy family that lives in a big, circular home. One morning, Mr. Smith woke up and saw a strawberry jam stain on his new carpet. He figured out that everyone who was there that morning had a jam sandwich. Billy Smith: \“I was outside playing basketball.\”The Maid: \“I was dusting the corners of the house.\”Chef: \“I was starting to make lunch for later.”Who is lying?\"",
-  ans: ["Billy", "Maid", "Chef"],
-  trueAns: "Maid"
-}]
+var qusAns = (function (app) {
+  var jsonObj = {};
+  var loadJSON = function (path) {
+    var xobj = newXMLHttpRequest();
+    xobj.overrideMineType('application/json');
+    xobj.open('GET', path);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState === 4) {
+        app.jsonObj = JSON.parse(xobj.responseText;)
+      }
+    };
+    xobj.send(null)
+  }
+})
 
 window.onclick = function () {
   document.querySelector('.overlay').classList.add("zero-opacity")
