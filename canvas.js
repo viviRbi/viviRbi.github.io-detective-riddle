d2c25a009141e557145e1fc7769939824bf828dc
VyLe <thaovyletruong@gmail.com>
2019-12-09 19:47:56 -0600

    finish css

---
+++ canvas.js
@@ -0,0 +1,20 @@
+
+// canvas = document.querySelector('canvas')
+
+
+
+// for (var i = 0; i < 10; i++) {
+//   var qus = new Image();
+//   qus.src = "qus.png"
+//   canvas.appendChild(qus)
+// }
+
+// var x = Math.floor(Math.random() * window.innerWidth + 5)
+//   var y = Math.floor(Math.random() * window.innerHeight + 5)
+//   var y = Math.floor(Math.random() * 6 + 1)
+//   qus[i].style.width = s
+//   qus[i].style.height = s
+//   qus[i].style.zIndex = Math.floor(Math.random() * 100 + 50)
+
+
+
--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 var quesAns = [{
+  title: "",
   ques: "The Smith family is a very wealthy family that lives in a big, circular home. One morning, Mr. Smith woke up and saw a strawberry jam stain on his new carpet. He figured out that everyone who was there that morning had a jam sandwich. Billy Smith: \“I was outside playing basketball.\”The Maid: \“I was dusting the corners of the house.\”Chef: \“I was starting to make lunch for later.”Who is lying?\"",
-  ans: ["Billy", "Maid", "Chef"]
+  ans: ["Billy", "Maid", "Chef"],
+  trueAns: "Maid"
 }]
+
+window.onclick = function () {
+  document.querySelector('.overlay').classList.add("zero-opacity")
+  this.setTimeout(function () {
+    document.querySelector('.overlay').style.display = "none"
+  }, 1000)
+}
