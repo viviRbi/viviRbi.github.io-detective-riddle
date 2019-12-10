0c50a3359d6014afeca7b972d6d4f90268ad8019
VyLe <thaovyletruong@gmail.com>
2019-12-10 00:04:03 -0600

    finish fetch Json, working on game logic

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 //------------------------------------ JSON
-var qusAns = (function (app) {
-  var jsonObj = {};
-  var loadJSON = function (path) {
-    var xobj = newXMLHttpRequest();
-    xobj.overrideMineType('application/json');
-    xobj.open('GET', path);
-    xobj.onreadystatechange = function () {
-      if (xobj.readyState === 4) {
-        app.jsonObj = JSON.parse(xobj.responseText);
-      }
-    };
-    xobj.send(null)
-  };
-  app.jsonObj = jsonObj;
-  app.loadJSON = loadJSON;
-  return app
-})(qusAns || {})
+
+fetch("./qus.json")
+  .then(response => response.json())
+  .then(data => displayQus(data))
+  .catch(err => console.log("err", err))
 
 //------------------------------------ Start Game
+
 window.onclick = function () {
   document.querySelector('.overlay').classList.add("zero-opacity")
   this.setTimeout(function () {
     document.querySelector('.overlay').style.display = "none"
   }, 1000)
 }
+
+//------------------------------------- Game play
+
+function displayQus(data) {
+  var dataText = data
+  var id = Math.floor(Math.random() * dataText.length - 1);
+  var qusText = document.querySelector('.ques-container h4')
+  qusText.innerText = dataText[id].qus
+  console.log(dataText[id].qus)
+  displayAns(dataText, id)
+}
+
+function displayAns(dataText, id) {
+  var ansContainer = document.querySelector('.ans-container')
+  for (let i = 0; i < dataText[id].quotes.length; i++) {
+
+    var ans = document.createElement('article')
+    ans.setAttribute('class', 'ans')
+    ansContainer.appendChild(ans)
+
+    var quoteText = document.createElement('h5')
+    ans.appendChild(quoteText)
+
+    quoteText.textContent = dataText[id].quotes[i]
+    console.log(dataText[id].quotes[i])
+  }
+}
+
