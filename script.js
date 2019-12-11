b71901194e06a78fc79e272809dfaf892f06af29
VyLe <thaovyletruong@gmail.com>
2019-12-11 17:01:28 -0600

    upade

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const finalScore = JSON.parse(localStorage.getItem('finalScore')) || []
 
 localStorage.setItem('finalScore', JSON.stringify(finalScore))
 
-// localStorage.clear()
+localStorage.clear()
 
 //------------------------------------ Fetch JSON -------------------
 
@@ -135,9 +135,9 @@ function reset(id, dataText) {
     }, 1000)
 
     //------------------------------- Local Storage
-
-    finalScore.push(score)
     localStorage.setItem('finalScore', JSON.stringify(finalScore))
+    finalScore.push(score)
+
 
     //------------------------------- Name board to Save high score
 
